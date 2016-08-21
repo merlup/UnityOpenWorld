93a79ef683e8a4e6269d6b3219809129b9fa12aa
Merlup <merlinjackson88@gmail.com>
2016-08-20 19:41:55 -0700

    first commit

---
+++ Assets/Boxcollider.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+
+}
+
+
+
+
+function  OnTriggerEnter (other : Collider) {
+
+if(other.gameObject.tag == "Player") {
+
+Debug.Log("Trigger Enter");
+
+}
+
+
+
+}
---
+++ Assets/Collider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
